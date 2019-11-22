11f0b91a8ae2c9170f37fe4970a344c0470261fc
Smirnov Alexey <a.v.smirnov@novotelecom.ru>
2019-11-22 21:37:59 +0700

    wip

--- Zabbix.py
+++ Zabbix.py
@@ -806,13 +806,12 @@ class ZabbixTrigger(Zabbix):
     def _get_last_events(self, since=None, limit=10, acknowledged=None, value=None):
         """Получение последних событий из Zabbix API по триггеру"""
         event_get = dict(
-            output=['clock', 'eventid', 'value', 'acknowledged', 'extend'],
+            output='extend',
             objectids=self._z_trigger['triggerid'],
             sortfield=['clock', 'eventid'],
             sortorder='DESC',  # сортировка от более нового к более старому
             limit=limit,
             select_acknowledges=['acknowledgeid', 'clock', 'message'],
-            filter={'acknowledges': {'message': '-'}},
         )
         if since is not None:
             event_get.update({
@@ -836,7 +835,7 @@ class ZabbixTrigger(Zabbix):
 
     def get_last_tickets_keys(self) -> Generator[str, None, None]:
         """Получение последних сообщений подтверждённых событий по триггеру из Zabbix API"""
-        z_events = self._get_last_events(acknowledged='True', value=1, since=int(time.time()) - (86400 * 7))
+        z_events = self._get_last_events(acknowledged=True, value=1, since=int(time.time()) - (86400 * 7))
         # Получаем список ключей тикетов
         return (e.get('message') for e in z_events if e.get('message'))
 
