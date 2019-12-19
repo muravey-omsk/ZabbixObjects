c769f93fa2e18d4f0ef061fccb49006029b5f1ee
Smirnov Alexey <a.v.smirnov@novotelecom.ru>
2019-12-19 17:40:51 +0700

    ProblemFactory: add get_by_id

--- ZabbixFactory.py
+++ ZabbixFactory.py
@@ -260,6 +260,9 @@ class ZabbixProblemFactory(ZabbixEventFactory):
         trigger = self._get_trigger_by_eventid(event['eventid'])
         return ZabbixProblem(trigger, event)
 
+    def get_by_id(self, eventid: int):
+        return ZabbixProblem.get_by_id(eventid)
+
     @zapi_exception("Ошибка получения Zabbix проблем")
     def get_by_groupids(self, groupids: List[int], limit: int = 500, **options):
         """Генератор событий из групп groupids по ZabbixAPI"""
