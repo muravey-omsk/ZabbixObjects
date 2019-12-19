3a5a1b9675f873dace878e60a589e70ad0c24d36
Smirnov Alexey <a.v.smirnov@novotelecom.ru>
2019-12-19 17:43:06 +0700

    ProblemFactory: fix get_by_id

--- ZabbixFactory.py
+++ ZabbixFactory.py
@@ -261,7 +261,7 @@ class ZabbixProblemFactory(ZabbixEventFactory):
         return ZabbixProblem(trigger, event)
 
     def get_by_id(self, eventid: int):
-        return ZabbixProblem.get_by_id(eventid)
+        return ZabbixProblem.get_by_id(self._zapi, eventid)
 
     @zapi_exception("Ошибка получения Zabbix проблем")
     def get_by_groupids(self, groupids: List[int], limit: int = 500, **options):
